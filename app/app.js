c77f6214c1923ad54251c3f5a1a12b542c67d876
Delaine Wendling <delaine.wendling@gmail.com>
2016-12-07 16:32:37 -0600

    Added home controller

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 "use strict";
 
-var app = angular.module("MyApp", []);
+var app = angular.module("MyApp", ['ngRoute'])
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeCtrl'
+  })
+  .otherwise('/');
+});
